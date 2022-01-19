8d40c2bdf7fb2df131213b0ac7f488082014d1e9
Shirene Boyd <shirenekadkhodai@gmail.com>
2022-01-19 11:13:44 -0600

    added Tuesday and past validation to reservation.controller.js

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,7 +1,6 @@
 const service = require("./reservations.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-
 /**
  * List handler for reservation resources
  */
@@ -49,12 +48,11 @@ function validationReservation(req, res, next) {
       status: 400,
       message: "people must be a number",
     });
-}
-
+  }
 
   /**
- * D a t e   Validation
- */
+   * D a t e   Validation
+   */
   const dateFormat = /\d\d\d\d-\d\d-\d\d/;
   const timeFormat = /\d\d:\d\d/;
   if (!data.reservation_date.match(dateFormat)) {
@@ -69,17 +67,31 @@ function validationReservation(req, res, next) {
       message: "reservation_time must be a time",
     });
   }
-  next();
-
-
-//let tuesdayCheck = new Date();
-if (data.reservation_date.getDate() === 3){
-  next({
-    status: 400,
-    message: "reservation_date cannot be Tuesday"
-  })
-
+  /**
+   * US-02 Tuesday Validation and Past Date Validation
+   */
+  let tuesdayCheck = new Date(data.reservation_date);
+  tuesdayCheck = tuesdayCheck.getUTCDay();
+  //console.log("date", tuesdayCheck)
+  if (tuesdayCheck === 2) {
+    next({
+      status: 400,
+      message:
+        "reservation_date cannot be Tuesday when the restaurant is closed",
+    });
   }
+
+  let now = new Date();
+  let reservationDateTime = new Date(
+    `${data.reservation_date}T${data.reservation_time}`
+  );
+  if (reservationDateTime < now) {
+    next({
+      status: 400,
+      message: "reservation_date must be in the future",
+    });
+  }
+  next();
 }
 
 module.exports = {
